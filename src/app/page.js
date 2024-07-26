bd7865e0ce1d894e0aa459ada2b91a69620ed1c5
Ihtesham Ahmad <128892067+Mrautonomous@users.noreply.github.com>
2024-07-26 15:05:55 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/navbar";
 import AboutSection from "@/components/AboutSection";
 import ProjectSection from "@/components/ProjectSection";
+import EmailSection from "@/components/EmailSection"
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectSection />
+        <EmailSection />
       </div>
     </main>
   );
