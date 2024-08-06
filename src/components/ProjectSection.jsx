387f17a9f5451fdcee1146a7e5156a8b2029ea55
Ihtesham Ahmad <128892067+Mrautonomous@users.noreply.github.com>
2024-08-06 20:04:45 +0500

    Update ProjectSection.jsx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -16,15 +16,15 @@ const ProjectSection = () => {
       <h2 className="text-center text-4xl font-bold text-white mt-4 ">
         My Projects
       </h2>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projectsData.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            description={project.description}
-            imgUrl={project.image}
-          />
-        ))}
+     
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
       </div>
     </>
   );
